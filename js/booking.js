8badff3bfd4a785e0cba579a452353a63b46299a
Annacletter <anna.cletter12@gmail.com>
2024-07-17 13:53:32 -0700

    Merged files

---
+++ js/booking.js
@@ -0,0 +1,9 @@
+document.getElementById('hotelBookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Hotel booking submitted!');
+});
+
+document.getElementById('restaurantReservationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Restaurant reservation submitted!');
+});
